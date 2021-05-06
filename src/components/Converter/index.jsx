7f8f91d844407846f0d7fc7521aa59cd0b0ca7f8
Mateus Alencar <mtauws@gmail.com>
2021-05-06 16:18:17 -0300

    Criação dos Inputs e Botão

--- src/components/Converter/index.jsx
+++ src/components/Converter/index.jsx
@@ -7,6 +7,17 @@ class Converter extends Component {
       <div className="container">
         <div className="converter">
           <h2>Convert Binary to Decimal</h2>
+          <div className="elements">
+            <div className="input-bin">
+              <input className="input" type="text" placeholder="Enter your binary number"/>
+            </div>
+            <div className="btn-convert">
+              <button>Converter</button>
+            </div>
+            <div className="input-dec">
+              <input className="input" type="text" placeholder="Result" disabled/>
+            </div>
+          </div>
         </div>
       </div>
     );
