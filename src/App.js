2f1a3ee9bf86f4dd198f9934877ac25cf943ee53
Mateus Alencar <mtauws@gmail.com>
2021-05-06 15:39:16 -0300

    Criação da Classe Converter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Converter from './components/Converter'
+
 class App extends Component {
   render() { 
     return (
-      <h1>App</h1>
+      <div>
+        <h1>App</h1>
+        <Converter />
+      </div>
     );
   }
 }
---
+++ src/components/Converter/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Converter extends Component {
+  render() { 
+    return (
+      <h1>Converter</h1>
+    );
+  }
+}
+ 
+export default Converter;
