1e2ac5e02d97c6c52824f84a6ec60fe33ddff3aa
Mateus Alencar <mtauws@gmail.com>
2021-05-06 19:55:21 -0300

    Verificando o tamanho do binary

--- src/components/Converter/index.jsx
+++ src/components/Converter/index.jsx
@@ -7,20 +7,37 @@ class Converter extends Component {
   constructor() {
     super();
     this.convert = this.convert.bind(this);
+    this.isValidLength = this.isValidLength.bind(this);
+  }
+
+  isValidLength() {
+    const binaryNumber = document.querySelector('.input').value;
+    if (binaryNumber.length <= 8) {
+      return false;
+    }
+    return true;
   }
 
   convert(e) {
     e.preventDefault();
     const binaryNumber = document.querySelector('.input').value;
-    if (binaryNumber.length > 8) {
-      console.log('Numero maior que 8');
-    } if (binaryNumber.includes(1) || binaryNumber.includes(1)) {
+    const inputBin = document.querySelector('.input-bin');
+    const spanInvalidNumber = document.createElement('span');
+
+    if (this.isValidLength()) {
+      document.querySelector('.input').style.border = '2px solid red';
+      spanInvalidNumber.innerText = 'Max length is 8';
+      spanInvalidNumber.style.display = 'block';
+      spanInvalidNumber.style.color = 'red';
+      spanInvalidNumber.style.paddingLeft = '10px'
+      spanInvalidNumber.style.paddingTop = '5px'
+      inputBin.appendChild(spanInvalidNumber);
+    } else {
       const dec = binToDec(binaryNumber);
       document.querySelector('.decimal').value = dec;
-    } else {
-      console.log('Insira 1 ou 0');
+      document.querySelector('.input').style.border = 'none';
+      spanInvalidNumber.style.innerHTML = '';
     }
-
   }
 
   render() { 
