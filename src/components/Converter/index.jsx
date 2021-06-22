d4a039db7aa517f9582ccc90596a307164df9d2e
Mateus Alencar <mtauws@gmail.com>
2021-06-22 20:23:10 -0300

    chore: added componentDidMount

--- src/components/Converter/index.jsx
+++ src/components/Converter/index.jsx
@@ -10,12 +10,31 @@ class Converter extends Component {
     this.isValidLength = this.isValidLength.bind(this);
   }
 
+  componentDidMount() {
+    document.querySelector('.input').addEventListener('keydown', function(ev) {
+      if(ev.keyCode !== 48 && ev.keyCode !== 49 && ev.keyCode !== 8) {
+        ev.preventDefault();
+      }
+    });
+  }
+
   isValidLength() {
     const binaryNumber = document.querySelector('.input').value;
     if (binaryNumber.length <= 8) {
       return false;
     }
     return true;
+  }
+
+  invalidNumberStyle(spanInvalidNumber, inputBin) {
+    document.querySelector('.input').style.border = '2px solid red';
+    spanInvalidNumber.innerText = 'Max length is 8';
+    spanInvalidNumber.style.display = 'block';
+    spanInvalidNumber.style.color = 'red';
+    spanInvalidNumber.classList.add('spanRed');
+    spanInvalidNumber.style.paddingLeft = '10px'
+    spanInvalidNumber.style.paddingTop = '5px'
+    inputBin.appendChild(spanInvalidNumber);
   }
 
   convert(e) {
@@ -25,21 +44,9 @@ class Converter extends Component {
     const spanInvalidNumber = document.createElement('span');
 
     if (this.isValidLength()) {
-      document.querySelector('.input').style.border = '2px solid red';
-      spanInvalidNumber.innerText = 'Max length is 8';
-      spanInvalidNumber.style.display = 'block';
-      spanInvalidNumber.style.color = 'red';
-      spanInvalidNumber.classList.add('spanRed');
-      spanInvalidNumber.style.paddingLeft = '10px'
-      spanInvalidNumber.style.paddingTop = '5px'
-      inputBin.appendChild(spanInvalidNumber);
+      this.invalidNumberStyle(spanInvalidNumber, inputBin);
     } else {
       const dec = binToDec(binaryNumber);
-      document.querySelector('.input').addEventListener('keydown', function(ev) {
-        if(ev.keyCode != 48 && ev.keyCode != 49 && ev.keyCode != 8) {
-          ev.preventDefault();
-        }
-      });
       document.querySelector('.decimal').value = dec;
       document.querySelector('.input').style.border = 'none';
       if (document.body.contains(document.querySelector('span'))) {
